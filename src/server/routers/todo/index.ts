237a37030dbe2cbac6de3daf07f35e53e3e4226a
Jerome Gallego <jeromegallego2@gmail.com>
2025-04-23 01:40:53 +0200

    create new endpoint get task by id

--- src/server/routers/todo/index.ts
+++ src/server/routers/todo/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@/generated/prisma';
-import { taskMutationSchema } from '@/server/routers/todo/types';
+import { taskCreationSchema, taskSchema } from '@/server/routers/todo/types';
 import { publicProcedure, router } from '@/server/trpc';
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 const prisma = new PrismaClient();
@@ -9,37 +10,55 @@ export const todoRouter = router({
 	getTasks: publicProcedure.query(async () => (
 		await prisma.todo.findMany()
 	)),
+	byId: publicProcedure
+    	.input(z.number())
+		.query(async opts => {
+			const id = opts.input;
+			const task = await prisma.todo.findUnique({
+				where: { id },
+			});
+			if (!task) {
+				throw new TRPCError({
+				code: 'NOT_FOUND',
+				message: `No task with id '${id}'`,
+				});
+			}
+			return task;
+		}),
 	createTask: publicProcedure
-		.input(taskMutationSchema)
+		.input(taskCreationSchema)
 		.mutation(async (opts) => {
 			const { input } = opts;
-			await prisma.todo.create({
+			const task = await prisma.todo.create({
 				data: {
 					text: input.text,
 					completed: input.completed,
 					createdAt: input.createdAt
 				}
 			})
+			return task;
 		}),
 	updateTask: publicProcedure
-		.input(taskMutationSchema)
+		.input(taskSchema)
 		.mutation(async (opts) => {
 			const { input } = opts;
-			await prisma.todo.update({
+			const task = await prisma.todo.update({
 				where: { 
 					id: input.id
 				},
 				data: {
 					...input
 				}
-			})
+			});
+			return task;
 		}),
 	deleteTask: publicProcedure
 		.input(z.number())
 		.mutation(async (opts) => {
 			const id = opts.input;
-			await prisma.todo.delete({
+			const task = await prisma.todo.delete({
 				where: { id }
 			})
+			return task;
 		})
 });
